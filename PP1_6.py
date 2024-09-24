444ca4b25420febc96d15180da9b24928b9a9439
Luxshan-Partheepan-ICD2O1-01 <luxshan.partheepan@student.tdsb.on.ca>
2024-09-24 22:23:04 +0000

    done

--- PP1_6.py
+++ PP1_6.py
@@ -9,30 +9,36 @@
 def q1():
   num1 = input("Input a number: ")
   num2 = input("Input a number: ")
+  num1 = int(num1)
+  num2 = int(num2)
   
-  Print(num + num2)
+  print(num1 + num2)
 
 # From input recieve two integers.  Output the quotient rounded down.
 
 def q2():
   num1 = input("Input a number: ")
   num2 = input("Input a number: ")
-  
-  print(num1/num2)
+  num1 = int(num1)
+  num2 = int(num2)
+  print(num1 // num2)
 
 # Output the phrase "hello Mr. Kalisz have you seen my work yet?"
 
 def q3():
-  print (hello Mr. Kalisz have you seen my work yet?)
+  print("hello Mr. Kalisz have you seen my work yet?")
 
 # From input recieve two numbers (can be decimal fractions).  
 # Output their result multiplied together.  Then round down to the nearest whole number
 
 def q4():
-  input("Input a number: ")
-  num2 = input("Input a number: ")
+  num1 = float(input("Enter the first number: "))
+  num2 = float(input("Enter the second number: "))
+  result = num1 * num2
+  roundednumber = int(result)
+  print("The result rounded down to the nearest whole number is:", roundednumber)
   
-  print (int(num1 * num2))
+ 
 
 q1()
 q2()
